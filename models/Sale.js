194ee218d4f981f9252bc7f07a2a3d4fe4f571a9
heshankav <heshankavinda2023@gmail.com>
2026-05-21 22:26:49 +0530

    Creat the Sale.js file

---
+++ models/Sale.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const saleItemSchema = new mongoose.Schema({
+    item: {
+        type: mongoose.Scheema.Types.ObjectId,
+        ref: 'Item',
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: 1
+    },
+    priceAtSale: {
+        type: Number,
+        required: true,
+    }
+}, {_id: false });
+
+const saleschema = new mongoose.Schema({
+    items: [saleItemSchema],
+    subtotal: {
+        type:Number,
+        required: true
+    },
+    discount: {
+        type: Number,
+        default: 0
+    },
+    total: {
+        type: Number,
+        required: true
+    },
+    cashier: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    }   
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('Sale', saleschema);
