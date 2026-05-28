d06708b7f5bacaf62298010523d59f00e8eebe5d
gwdndevindi <gosthinawadugedinithi@gmail.com>
2026-05-29 03:43:17 +0530

    login page

---
+++ Login-system/src/login.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import './login.css';
+
+function Login(){
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <div style={{ display: 'flex' , height: '100vh' }}>
+
+            <div style = {{ flex: 1 }}>
+                <img src="canteen1.jpg" alt="Canteen1" style={{ width: '100%', height: '100%', objectFit: 'cover'}}/>
+            </div>
+
+            <div className="right-side">
+                <div className="login-card">
+                   <span style={{fontSize: '50px', marginTop: '40px' }}>🥗🍔</span>
+                   <h1><b>Smart Canteen</b></h1>
+                   <p>Sales Management System</p>
+                   <br></br>
+                   <h3><b>Login to your account</b></h3>
+
+                   <div className="input-container">
+                     <input
+                        type=""
+                        placeholder='👤 Username'
+                        value={username}
+                        onChange={(e)=> setUsername(e.target.value)}
+                     />
+                     <br></br>
+
+                     <br></br>
+                     <input
+                        type=""
+                        placeholder='🔑 Password'
+                        value={username}
+                        onChange={(e)=> setPassword(e.target.value)}
+                     />
+
+                   </div>
+                   <div className="remember-container">
+                     <input type="checkbox" id="rememberMe"/>
+                     <label htmlFor="rememberMe">Remember me</label>
+
+                     <       a href="#forgot" className="forgot-link">Forgot Password?</a>
+
+                   </div>
+                </div>
+            </div>
+
+        </div>
+          
+    );
+
+}
+
+export default Login;
